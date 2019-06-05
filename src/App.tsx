7421a630de411b69be595c8f795d61ffa29130c3
jamesliudotcc <james@jamesliu.cc>
2019-06-04 21:57:26 -0700

    app console logs button click to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { any } from 'prop-types';
 
 type Digits = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0;
 type Operators = '+' | '-' | '×' | '÷' | '=';
@@ -9,24 +10,24 @@ const App: React.FC = () => {
   //prettier-ignore
   const keyBoard: Button[] = [0, 1, 3, 4, 5, 6, 7, 8, 9, '+', '-', '×', '÷', '='];
 
-  const handleClick = () => {
-    console.log('hi');
-  };
-
   const buttons = keyBoard.map((eachKey, i) => (
-    <Button value={eachKey} key={`button-${eachKey}`} onClick={handleClick} />
+    <Button value={eachKey} key={`button-${eachKey}`} />
   ));
 
   return <div className="App">{buttons}</div>;
 };
 
-const Button: React.FC<{ value: Button; onClick: any }> = props => {
+const Button: React.FC<{ value: Button }> = props => {
+  const handleClick = (something: Button) => {
+    console.log(something);
+  };
   return (
-    // @ts-ignore
-    <button id="{props.value}" onClick={props.onClick}>
+    <button id="{props.value}" onClick={() => handleClick(props.value)}>
       {props.value}
     </button>
   );
 };
 
 export default App;
+
+// TODO: Get this working in React without Typescript
