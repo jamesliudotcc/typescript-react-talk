df2399475e6600cad4583c7fda131cfe597bf6f6
jamesliudotcc <james@jamesliu.cc>
2019-06-04 20:39:59 -0700

    Buttons display on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
 
+type Digits = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0;
+type Operators = '+' | '-' | '×' | '÷' | '=';
+type Button = Digits | Operators;
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //prettier-ignore
+  const keyBoard: Button[] = [0, 1, 3, 4, 5, 6, 7, 8, 9, '+', '-', '×', '÷', '='];
+
+  const handleClick: any = () => {
+    console.log('hi');
+  };
+
+  const buttons = keyBoard.map((eachKey, i) => (
+    <Button value={eachKey} key={`button-${eachKey}`} />
+  ));
+
+  return <div className="App">{buttons}</div>;
+};
+
+const Button: React.FC<{ value: Button }> = props => {
+  return <button id="{props.value}">{props.value}</button>;
+};
 
 export default App;
